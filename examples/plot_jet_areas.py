2a7e06df0c51cc9fadbe10121c479bb2d7ace87c
Noel Dawe <noel.dawe@gmail.com>
2017-05-28 01:02:47 +1000

    comments

--- examples/plot_jet_areas.py
+++ examples/plot_jet_areas.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 from matplotlib.colors import LinearSegmentedColormap
 
+# define eta and phi ranges and number of bins along each axis
 eta_min, eta_max = -4., 4.
 extent = eta_min, eta_max, -np.pi, np.pi
 bins = 200
 
 event = ep2ptepm(get_event())
+
 # create regular grid of ghosts
 eta_edges = np.linspace(eta_min, eta_max, bins + 1)
 phi_edges = np.linspace(-np.pi, np.pi, bins + 1)
@@ -21,6 +23,7 @@ ghosts = np.zeros(eta.shape[0] * phi.shape[0], dtype=DTYPE_PTEPM)
 ghosts['pT'] = 1e-8
 ghosts['eta'] = X.ravel()
 ghosts['phi'] = Y.ravel()
+
 # add ghosts to the event
 event = np.concatenate([event, ghosts], axis=0)
 
@@ -30,8 +33,9 @@ ax = None
 for p, label in zip((-1, 0, 1), (r'anti-$k_t$', 'Cam/Aachen', '$k_t$')):
     # cluster
     sequence = cluster(event, R=1.0, p=p)
-    # plot jet areas
     jets = sequence.inclusive_jets(ptmin=10)
+
+    # pick a different color for each jet
     colors = cm.rainbow(np.linspace(0, 1, len(jets)))
     cmap = LinearSegmentedColormap.from_list('cmap', colors, len(colors))
     ax = fig.add_subplot(1, 3, p + 2, sharey=ax)
@@ -42,9 +46,12 @@ for p, label in zip((-1, 0, 1), (r'anti-$k_t$', 'Cam/Aachen', '$k_t$')):
         jetarea, _, _ = np.histogram2d(constit['eta'], constit['phi'],
                                        bins=(eta_edges, phi_edges))
         area += (jetarea > 0) * (ijet + 1)
+
+    # plot all areas
     ax.imshow(np.ma.masked_where(area == 0, area).T, cmap=cmap,
               extent=extent, aspect=(eta_max - eta_min) / (2*np.pi),
               interpolation='none', origin='lower')
+
     # overlay original event
     particles = ep2ptepm(get_event())
     ax.scatter(particles['eta'], particles['phi'],
