8f43571b3512e437f466b0604486e9ae2e197997
Noel Dawe <noel.dawe@gmail.com>
2017-05-28 00:10:26 +1000

    example

--- example.py
+++ example.py
@@ -21,26 +21,52 @@ print("\nor (E, px, py, pz):")
 print(jets[5].constituents_array(ep=True))
 
 # plot the jet areas
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+from matplotlib.colors import LinearSegmentedColormap, LogNorm
+
+eta_min, eta_max = -4., 4.
+extent = eta_min, eta_max, -np.pi, np.pi
+
 event = ep2ptepm(get_event())
-# add grid of infinitely soft particles
-eta = np.linspace(-5, 5, 100)
-phi = np.linspace(-np.pi, np.pi, 100)
+# create regular grid of ghosts
+eta_edges = np.linspace(eta_min, eta_max, 101)
+phi_edges = np.linspace(-np.pi, np.pi, 101)
+eta = np.linspace(eta_min, eta_max, 101)[:-1] + (eta_max - eta_min) / 200
+phi = np.linspace(-np.pi, np.pi, 101)[:-1] + (2*np.pi / 200)
 X, Y = np.meshgrid(eta, phi)
 ghosts = np.zeros(eta.shape[0] * phi.shape[0], dtype=DTYPE_PTEPM)
 ghosts['pT'] = 1e-8
 ghosts['eta'] = X.ravel()
 ghosts['phi'] = Y.ravel()
+# add ghosts to the event
 event = np.concatenate([event, ghosts], axis=0)
-sequence = cluster(event, R=0.6, p=-1)
-jets = sequence.inclusive_jets(ptmin=1)
 
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
+fig = plt.figure(figsize=(9, 3))
 
-fig = plt.figure(figsize=(6, 6))
-ax = fig.add_subplot(111)
-constits = jets[1].constituents_array()
-ax.hist2d(constits['eta'], constits['phi'], bins=(eta, phi), weights=constits['pT'],
-          norm=LogNorm(vmin=constits['pT'].min(), vmax=constits['pT'].max()))
+for p in (-1, 0, 1):
+    # cluster
+    sequence = cluster(event, R=1.0, p=p)
+    # plot jet areas
+    jets = sequence.inclusive_jets(ptmin=10)
+    colors = cm.rainbow(np.linspace(0, 1, len(jets)))
+    cmap = LinearSegmentedColormap.from_list('cmap', colors, len(colors))
+    ax = fig.add_subplot(1, 3, p + 2)
+    area = np.zeros((eta_edges.shape[0] - 1, phi_edges.shape[0] - 1),
+                    dtype=np.float64)
+    for ijet, jet in enumerate(jets):
+        constit = jet.constituents_array()
+        jetarea, _, _ = np.histogram2d(constit['eta'], constit['phi'],
+                                       bins=(eta_edges, phi_edges))
+        area += (jetarea > 0) * (ijet + 1)
+    ax.imshow(np.ma.masked_where(area == 0, area).T, cmap=cmap,
+              extent=extent, aspect=(eta_max - eta_min) / (2*np.pi),
+              interpolation='none', origin='lower')
+    # overlay original event
+    particles = ep2ptepm(get_event())
+    ax.scatter(particles['eta'], particles['phi'],
+               s=30 * particles['pT'] / particles['pT'].max())
+    ax.set_xlim(extent[:2])
+    ax.set_ylim(extent[2:])
 fig.tight_layout()
 fig.savefig('jets.png')
