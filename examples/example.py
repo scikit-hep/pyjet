3ef6c29422eaa7e2124f8a8e1ad1b06bf43fc9d4
Noel Dawe <noel.dawe@gmail.com>
2017-05-29 14:28:40 +1000

    demo of userinfo in example.py

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,29 @@
 from pyjet import cluster
 from pyjet.testdata import get_event
+from numpy.lib.recfunctions import append_fields
+from numpy.testing import assert_array_equal
+import numpy as np
 
-sequence = cluster(get_event(), R=0.6, p=-1, ep=True)
+# event's dtype=np.dtype([('E', 'f8'), ('px', 'f8'), ('py', 'f8'), ('pz', 'f8')])
+# this is the sample event shipped with FastJet with E moved to the first column
+event = get_event()
+
+# You can associate arbitrary additional information to each particle
+# and this information can be accessed as attributes of the PyPseudoJets
+event = append_fields(event, 'id', data=np.arange(len(event)))
+
+sequence = cluster(event, R=0.6, p=-1, ep=True)
 jets = sequence.inclusive_jets()
 
+ids = []
+for jet in jets:
+    for constit in jet:
+        ids.append(constit.id)
+ids.extend([p.id for p in sequence.unclustered_particles()])
+# Are all particles accounted for?
+assert_array_equal(sorted(ids), np.arange(len(event)))
+
+# Printing a few things here as a demonstration of the basic functionality
 print("{0: <5} {1: >10} {2: >10} {3: >10} {4: >10} {5: >10}".format(
     "jet#", "pT", "eta", "phi", "mass", "#constit."))
 for i, jet in enumerate(jets[:6]):
