e0c04c73dfacc34980cea393e7a228e514f7ed10
Domen <13206830+domen13@users.noreply.github.com>
2021-01-16 21:17:24 +0100

    Add test for recombination schemes

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -46,6 +46,29 @@ def test_cluster_vectors_wrong_type():
         vectors = np.zeros(10, dtype=[('a', 'f8'), ('b', 'f8'), ('c', 'f4'), ('d', 'f8')])
         cluster(vectors, R=0.6, p=-1)
 
+def test_cluster_recombination_schemes():
+    recomb_schemes = {
+        'E_scheme': (983.28, -0.8676, 36.46),
+        'pt_scheme': (983.52, -0.8672, 0.00),
+        'pt2_scheme': (983.52, -0.8679, 0.00),
+        'Et_scheme': (983.55, -0.8672, 0.00),
+        'Et2_scheme': (983.55, -0.8679, 0.00),
+        'BIpt_scheme': (983.52, -0.8671, 0.00),
+        'BIpt2_scheme': (983.52, -0.8679, 0.00),
+        'WTA_pt_scheme': (983.52, -0.8684, 0.14),
+        'WTA_modp_scheme': (983.03, -0.8684, 0.14),
+    }
+
+    for recomb_scheme, jet in recomb_schemes.items():
+        sequence = cluster(get_event(), R=0.6, p=-1, recomb_scheme=recomb_scheme)
+        jets = sequence.inclusive_jets()
+        
+        assert jets[0].pt == approx(jet[0], abs=1e-2)
+        assert jets[0].eta == approx(jet[1], abs=1e-4)
+        assert jets[0].mass == approx(jet[2], abs=1e-2)
+
+    with pytest.raises(ValueError):
+        sequence = cluster(get_event(), R=0.6, p=-1, recomb_scheme='invalid_scheme')
 
 def test_userinfo():
     event = get_event()
